bfbfff71b28b39ca1abbe797b05e59d254033692
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2024-08-10 14:49:34 +0200

    Add model tests

---
+++ tests/device/models_test.py
@@ -0,0 +1,14 @@
+from device_monitor.device.models import Device
+
+
+class TestModels:
+    def test_device_model(self):
+        device = Device(
+            id=123,
+            expected_fields=["current", "voltage"],
+            readings={"current": 123, "voltage": 234},
+        )
+
+        assert device.id == 123
+        assert device.expected_fields == ["current", "voltage"]
+        assert device.readings == {"current": 123, "voltage": 234}
