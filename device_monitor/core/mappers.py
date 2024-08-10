4a1d9fa427c18ff2de41995f0ace7c13c75f3bf8
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2024-08-10 21:07:41 +0200

    Exclude pass from coverage in abstract functions

--- device_monitor/core/mappers.py
+++ device_monitor/core/mappers.py
@@ -8,9 +8,9 @@ class AbstractMapper(ABC, Generic[ModelType]):
     @staticmethod
     @abstractmethod
     def dict_to_model(data: dict[str, Any]) -> ModelType:
-        pass
+        pass  # pragma: no cover
 
     @staticmethod
     @abstractmethod
     def model_to_dict(model: ModelType) -> dict[str, Any]:
-        pass
+        pass  # pragma: no cover
